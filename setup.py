c8347d2a8e62fe90c28d905b343a3e6584addb8b
Ruy Asan <ruy@demonware.net>
2012-03-16 18:24:24 +0000

    Added dist info

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='treecompare',
+      version='1.0',
+      description="""
+      	Compare complex trees of objects with useful 
+      	diff output and fuzzy matching options.""",
+      author='Ruy Asan',
+      author_email='ruyasan@gmail.com',
+      url='https://github.com/rubyruy/treecompare',
+      install_requires=['distribute'],
+     )
